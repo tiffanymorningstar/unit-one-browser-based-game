9dc7912f6275650b9d9857957f253fe73b410698
trmorningstar <trmorningstar@gmail.com>
2022-09-09 15:56:24 -0400

    add pseudo code and template

---
+++ js/app.js
@@ -0,0 +1,68 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+
+//Everything you put in Cached element references, gives you access to your html
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+//Bubbling -We can add an event listener to a top level object, and it can access the individual elements that are nested in the parent element.  Example: 
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+init()
+//initialize the game, always put it for interaction on game
+
+
+// PSEUDO CODE
+// Map out basic story, determine how many assets needed
+// Find images and music
+// Upload images on image hosting site
+// Load music file/s into asset folder
+// Create variables for:
+// Winner
+// Loser
+// Story Progress
+// Create data file for story text
+// Add HTML for:
+// Each story image (one for each step in storyline)
+// Start button
+// Advancing story buttons
+// Reset button
+// Welcome message text
+// Game start prompt text
+// Data file for story text
+// Animation 
+// Create functions for: 
+// Init (settings for a new game)
+// Determining when there is a winner
+// Rendering a winner 
+// Rendering text changes
+// Rendering Image changes
+// Add cached element references for:
+// Start button
+// 2 Buttons for advancing story (two different options at each step)
+// Images changing when advancing story buttons are clicked
+// Story text changing when advancing story buttons are clicked 
+// Animation playing when player wins 
+// Music playing when advancing story buttons are clicked
+// Congratulatory message displaying when player wins
+// Sorry message playing when player loses
+// Add event listeners for:
+// Start button
+// Advancing story buttons (2 at each step)
+// Reset button
+// Add google fonts
+// Add favicon to site
